a9f60b5017df3039ae644ab2dab8ebac76d149d3
Evin Sellin <evinism@gmail.com>
2022-07-18 22:56:18 -0700

    aaa

--- pages/jarquest.tsx
+++ pages/jarquest.tsx
@@ -48,28 +48,27 @@ const JarQuest = () => {
         setBasicJars(basicJars - 1);
         setMoney(money + 1);
       }
-      if (Math.random() > 0.9) {
-        setDeluxeJars(basicJars - 1);
+    }
+    if (shopOpenForBusiness && deluxeJars > 0) {
+      if (Math.random() > 0.8) {
+        setBasicJars(deluxeJars - 1);
         setMoney(money + 10);
       }
     }
   }, 100);
 
-  const canMakeBasicJar = () => {
-    return sandKgs >= 10 && inventory.includes(furnace);
-  };
+  const canMakeBasicJar = sandKgs >= 10 && inventory.includes(furnace);
   const makeBasicJar = () => {
-    if (canMakeBasicJar()) {
+    if (canMakeBasicJar) {
       setSandKgs(sandKgs - 10);
       setBasicJars(basicJars + 1);
     }
   };
 
-  const canMakeDeluxeJar = () => {
-    return sandKgs >= 10 && inventory.includes(furnace) && learnedAboutJars;
-  };
+  const canMakeDeluxeJar =
+    sandKgs >= 10 && inventory.includes(furnace) && learnedAboutJars;
   const makeDeluxeJar = () => {
-    if (canMakeBasicJar()) {
+    if (canMakeBasicJar) {
       setSandKgs(sandKgs - 10);
       setDeluxeJars(deluxeJars + 1);
     }
@@ -78,6 +77,66 @@ const JarQuest = () => {
   return (
     <article>
       <h1>Jar Quest</h1>
+      <div>
+        {inventory.length === 0 && (
+          <button onClick={() => setInventory([shovel])}>
+            Steal your dad's shovel
+          </button>
+        )}
+        {inventory.includes(shovel) && (
+          <button onClick={() => setSandKgs(sandKgs + 1)}>
+            Scoop some Sand
+          </button>
+        )}
+        {!inventory.includes(furnace) && sandKgs >= 10 && (
+          <button
+            onClick={() => {
+              setInventory([...inventory, furnace]);
+              setSandKgs(sandKgs - 10);
+            }}
+          >
+            Build a Furnace out of Sand
+          </button>
+        )}
+        {canMakeBasicJar && (
+          <button onClick={makeBasicJar}>Make a Basic Jar</button>
+        )}
+        {basicJars && (
+          <button
+            onClick={() => {
+              setBasicJars(basicJars - 1);
+              setJarsBroken(jarsBroken + 1);
+            }}
+          >
+            Break a jar because it's not good enough.
+          </button>
+        )}
+        {basicJars >= 5 && (
+          <button onClick={() => setShopOpenForBusiness(true)}>
+            Incorporate JarCo and open for business.
+          </button>
+        )}
+        {jarsBroken > 100 && !learnedAboutJars && (
+          <button onClick={() => setLearnedAboutJars(true)}>
+            By breaking jars, they yield their secrets to you. Learn from your
+            jars.
+          </button>
+        )}
+        {money > 10 && !inventory.includes(autoscooper) && (
+          <button
+            onClick={() => {
+              setMoney(money - 10);
+              setInventory([...inventory, autoscooper]);
+            }}
+          >
+            Buy an Autoscooper
+          </button>
+        )}
+
+        {canMakeDeluxeJar && (
+          <button onClick={makeDeluxeJar}>Make a Deluxe Jar</button>
+        )}
+      </div>
       <table>
         <thead>
           <tr>
@@ -130,55 +189,6 @@ const JarQuest = () => {
           ))}
         </tbody>
       </table>
-      <div>
-        {inventory.length === 0 && (
-          <button onClick={() => setInventory([shovel])}>
-            Steal your dad's shovel
-          </button>
-        )}
-        {inventory.includes(shovel) && (
-          <button onClick={() => setSandKgs(sandKgs + 1)}>
-            Scoop some Sand
-          </button>
-        )}
-        {!inventory.includes(furnace) && sandKgs >= 10 && (
-          <button
-            onClick={() => {
-              setInventory([...inventory, furnace]);
-              setSandKgs(sandKgs - 10);
-            }}
-          >
-            Build a Furnace out of Sand
-          </button>
-        )}
-        {canMakeBasicJar && (
-          <button onClick={makeBasicJar}>Make a Basic Jar</button>
-        )}
-        {basicJars && (
-          <button
-            onClick={() => {
-              setBasicJars(basicJars - 1);
-              setJarsBroken(jarsBroken + 1);
-            }}
-          >
-            Break a jar because it's not good enough.
-          </button>
-        )}
-        {shopOpenForBusiness && (
-          <button onClick={() => setShopOpenForBusiness(true)}>
-            Incorporate JarCo and open for business.
-          </button>
-        )}
-        {jarsBroken > 100 && !learnedAboutJars && (
-          <button onClick={() => setLearnedAboutJars(true)}>
-            By breaking jars, they yield their secrets to you. Learn from your
-            jars.
-          </button>
-        )}
-        {canMakeDeluxeJar && (
-          <button onClick={makeDeluxeJar}>Make a Deluxe Jar</button>
-        )}
-      </div>
     </article>
   );
 };
