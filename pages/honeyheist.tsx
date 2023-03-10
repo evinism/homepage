86f2c283480df8a2fb1ee87c34115c71686163d6
Evin Sellin <evinism@gmail.com>
2023-03-09 23:27:13 -0800

    moar bear stuff

--- pages/honeyheist.tsx
+++ pages/honeyheist.tsx
@@ -103,13 +103,39 @@ const CriminalBearChart = () => {
     }
   };
 
+  const rollForBearAdvantage = () => {
+    const roll1 = Math.floor(Math.random() * 6) + 1;
+    const roll2 = Math.floor(Math.random() * 6) + 1;
+    const roll = Math.min(roll1, roll2);
+    if (roll <= bearAmount) {
+      window.alert(
+        `You rolled a ${roll1} and a ${roll2}, taking the lower (${roll}), and succeeded! Amazing Bear! (needed ${bearAmount} or below)`
+      );
+    } else {
+      window.alert(
+        `You rolled a ${roll1} and a ${roll2}, taking the lower (${roll}), and failed! Drat! (needed ${bearAmount} or below)`
+      );
+    }
+  };
+
+  const rollForCrimeAdvantage = () => {
+    const roll1 = Math.floor(Math.random() * 6) + 1;
+    const roll2 = Math.floor(Math.random() * 6) + 1;
+    const roll = Math.min(roll1, roll2);
+    if (roll <= criminalAmount) {
+      window.alert(
+        `You rolled a ${roll1} and a ${roll2}, taking the lower (${roll}), and succeeded! Crime Pays! (needed ${criminalAmount} or below)`
+      );
+    } else {
+      window.alert(
+        `You rolled a ${roll1} and a ${roll2}, taking the lower (${roll}), and failed! Drat! (needed ${criminalAmount} or below)`
+      );
+    }
+  };
+
   return (
     <div className={styles.bearCrimeChartBlock}>
       <div>Bear Crime Chart</div>
-      <div className={styles.rollItems}>
-        <button onClick={rollForBear}>Roll For Bear</button>
-        <button onClick={rollForCrime}>Roll For Crime</button>
-      </div>
       <div className={styles.bearCrimeOuterDiagram}>
         <div className={styles.bearCrimeMiddleRow}>
           <button onClick={toBear}>MORE BEAR</button>
@@ -123,6 +149,17 @@ const CriminalBearChart = () => {
             <div>Crime Stat: {criminalAmount} / 6</div>
           </div>
         </div>
+      </div>
+      <div>Dice Rollers!</div>
+      <div className={styles.rollItems}>
+        <button onClick={rollForBear}>Roll For Bear</button>
+        <button onClick={rollForBearAdvantage}>
+          Roll For Bear (Advantage)
+        </button>
+        <button onClick={rollForCrime}>Roll For Crime</button>
+        <button onClick={rollForCrimeAdvantage}>
+          Roll For Crime (Advantage)
+        </button>
       </div>
     </div>
   );
@@ -212,7 +249,7 @@ const characterToDescription = (character: Character): string => {
     character.bearType
   } who is the ${character.role} of the group! They wear a ${
     character.hat
-  } on their head. As a ${character.bearType}, their special ability is "${
+  } on their head. As a ${character.bearType}, your special ability is "${
     skills[character.bearType]
   }".`;
 };
