6e406084f89678d4e2255b716da214b4c529417e
Evin Sellin <evin.sellin@discordapp.com>
2023-11-12 15:50:07 -0800

    add netlify identity widget

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import "../styles/global.css";
 import { GoogleAnalytics } from "nextjs-google-analytics";
-
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
       <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
     </>
