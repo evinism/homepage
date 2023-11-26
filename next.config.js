490bb3ff3f38b7f5fb72fb576e3e08326499616a
Evin Sellin <evin.sellin@discordapp.com>
2023-11-26 15:44:20 -0800

    no-export?

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 const nextConfig = {
-  output: "export",
-
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
-
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
   // skipTrailingSlashRedirect: true,
-
   // Optional: Change the output directory `out` -> `dist`
-  distDir: ".next",
 };
 
 module.exports = nextConfig;
