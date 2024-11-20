66f2c734447b525a4d1f47a3352b71a5ed5ceb3c
Evin Sellin <evinism@gmail.com>
2024-11-19 22:01:27 -0800

    stuff

---
+++ pages/dmitry.tsx
@@ -0,0 +1,10 @@
+const FireLinks = () => {
+  return (
+    <article>
+      sup! Don't ever go to grad school. It's a scam.
+      <p>I love you. I'm sorry. Please forgive me. Thank you.</p>
+    </article>
+  );
+};
+
+export default FireLinks;
