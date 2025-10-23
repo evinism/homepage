d08d0618a2bdb48b4b65319b6b7f20b9ae383b7c
Evin Sellin <evinism@gmail.com>
2025-10-23 13:23:36 -0700

    head

--- pages/qr.tsx
+++ pages/qr.tsx
@@ -2,34 +2,53 @@ import { Button, Input, InputLabel } from "@mui/material";
 import { useEffect, useRef, useState } from "react";
 import QRCode from "qrcode";
 import styles from "./qr.module.css";
+import Head from "next/head";
 
 const QR = () => {
   const [text, setText] = useState("");
   return (
-    <main className={styles.page}>
-      <h1>QR Code Generator</h1>
-      <Input
-        type="text"
-        value={text}
-        placeholder="Enter text to encode"
-        onChange={(e) => setText(e.target.value)}
-        fullWidth
-      />
-      {
-        text && (<>
-          <DisplayQR text={text} />
-          <div>
-            <Button onClick={async () => {
-              const dataURL = await QRCode.toDataURL(text);
-              const a = document.createElement('a');
-              a.href = dataURL;
-              a.download = 'qrcode.png';
-              a.click();
-            }}>Download</Button>
-          </div>
+    <>
+      <Head>
+        <title>QR Code Generator</title>
+        <meta
+          name="description"
+          content="Generate raw QR codes. No tracking, no data collection."
+        />
+        <meta property="og:title" content="QR Code Generator" />
+        <meta
+          property="og:description"
+          content="Generate raw QR codes. No tracking, no data collection."
+        />
+      </Head>
+      <main className={styles.page}>
+        <h1>QR Code Generator</h1>
+        <Input
+          type="text"
+          value={text}
+          placeholder="Enter text to encode"
+          onChange={(e) => setText(e.target.value)}
+          fullWidth
+        />
+        {text && (
+          <>
+            <DisplayQR text={text} />
+            <div>
+              <Button
+                onClick={async () => {
+                  const dataURL = await QRCode.toDataURL(text);
+                  const a = document.createElement("a");
+                  a.href = dataURL;
+                  a.download = "qrcode.png";
+                  a.click();
+                }}
+              >
+                Download
+              </Button>
+            </div>
           </>
         )}
-    </main>
+      </main>
+    </>
   );
 };
 
